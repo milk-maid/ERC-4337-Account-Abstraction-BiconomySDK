b552ec4bb22ced81c804413eb1e21f6c1362a46a
milk-maid <roqbell@gmail.com>
2023-11-02 23:13:36 +0100

    overwrite NextJS Webpack config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
