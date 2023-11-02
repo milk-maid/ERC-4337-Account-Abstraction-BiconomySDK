e4edd2109a1e632273402c3beae066b46189fd83
milk-maid <roqbell@gmail.com>
2023-11-02 23:18:40 +0100

    write the logic for Biconomy's Social Login

---
+++ src/components/Wallet.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useState } from "react";
+import SocialLogin from "@biconomy/web3-auth";
+
+export default function Wallet() {
+  const sdkRef = useRef<SocialLogin | null>(null);
+  const [interval, enableInterval] = useState<boolean>(false);
+
+  // login() function
+  // setupSmartAccount() function
+
+  useEffect(() => {
+    let configureLogin: NodeJS.Timeout | undefined;
+    if (interval) {
+      configureLogin = setInterval(() => {
+        if (!!sdkRef.current?.provider) {
+          // setupSmartAccount();
+          clearInterval(configureLogin);
+        }
+      }, 1000);
+    }
+  }, [interval]);
+}
